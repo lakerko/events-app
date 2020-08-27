8b37081bcd9f99c8f75dc5ff78d0fa1f0c06a14a
Lakerko <martin.ockovsky@gmail.com>
2020-08-27 12:44:04 +0200

    feat: init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'events-app';
+  // Create events module
+  // -- events service
+  // ---- request events (upcoming, past) from server
+  // ---- create events
+  // -- events dashboard component
+  // ---- routing for upcoming and past events
+  // ----- get data via service, sends them to list component
+  // -- events-list component
+  // ---- display input events in a list
+  // -- event-list-item component
+  // ----- display data of a specific event in a list
+  // -- event-detail-page component
+  // ---- request data about specific event based on url id
+  // ---- display event informations
+  // -- event-create-page component
+  // ---- create form
+  // ---- call create request from service
 }
