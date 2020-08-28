21ce2d37fafd183ca67406905a2833216576174c
Lakerko <martin.ockovsky@gmail.com>
2020-08-28 17:35:31 +0200

    feat: add layout and navigation in events-page

--- src/app/events/events-page/events-page.component.ts
+++ src/app/events/events-page/events-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationItem } from '../models/navigation.interface';
 
 @Component({
   selector: 'app-events-page',
   templateUrl: './events-page.component.html',
-  styleUrls: ['./events-page.component.scss']
+  styleUrls: ['./events-page.component.scss'],
 })
 export class EventsPageComponent implements OnInit {
+  public navigationItems: NavigationItem[] = [
+    {
+      label: 'upcoming',
+      path: ['upcoming'],
+    },
+    {
+      label: 'past',
+      path: ['past'],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
