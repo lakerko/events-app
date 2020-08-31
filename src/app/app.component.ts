db41ebe09f050911bedab3d164587dc5cc4e5133
Lakerko <martin.ockovsky@gmail.com>
2020-08-31 18:09:29 +0200

    feat: add loading indicator to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,21 +14,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // Create events module
-  // -- events service
-  // ---- request events (upcoming, past) from server
-  // ---- create events
-  // -- events dashboard component
-  // ---- routing for upcoming and past events
-  // ----- get data via service, sends them to list component
-  // -- events-list component
-  // ---- display input events in a list
-  // -- event-list-item component
-  // ----- display data of a specific event in a list
-  // -- event-detail-page component
-  // ---- request data about specific event based on url id
-  // ---- display event informations
-  // -- event-create-page component
-  // ---- create form
-  // ---- call create request from service
+  public loading = true;
+
+  constructor(private readonly router: Router) {
+    this.router.events.subscribe((routerEvent: RouterEvent) => {
+      this.setLoadingBasedOnRouterState(routerEvent);
+    });
+  }
+
+  setLoadingBasedOnRouterState(routerEvent: RouterEvent): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 }
